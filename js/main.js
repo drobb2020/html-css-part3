7dd3289aaf87dd40d4fcdd1281c1843ddb10dfbd
David Robb <66834033+drobb2020@users.noreply.github.com>
2021-10-02 19:38:37 -0400

    Initial commit - build complete

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const { ["log"]: c } = console;
+
+c('Hello World!');
+
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
